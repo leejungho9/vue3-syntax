1f17a011966e55a5e60a2d8cca71da7be9c83843
leejungho9 <9wjdgh9@naver.com>
2022-01-05 20:20:04 +0900

    add Watch

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
 <template>
-  <button @click="add">
-    ADD
-  </button>
-  <h1> {{ reversedMessage }}</h1>
+  <h1 @click="changeMessage">
+    {{ msg }}
+  </h1>
   <h1> {{ reversedMessage }}</h1>
 </template>
 
@@ -10,30 +9,26 @@
 export default {
   data () {
     return { 
-      // 값을 얻어내는 용도(Getter) / 값을 지정하는 용도 (Setter) 다 가능
-      msg : "hello Computed"
+      msg : "Hello?"
     }
   },
-  computed : { //캐싱기능이 있어 이미 연산된 기록이 있으면 저장된 값을 화면에 출력해줌
-  //  (Getter)읽기 전용 ... 반응형으로 동작할 수는 없음 / 값을 얻어내는 용도로만 사용
-  //  reversedMessage () { 
-  //    this.msg.split('').reverse().join('' )
-  //   }
-  // Getter , Setter 
-  reversedMessage : {
-    get() {
-      return this.msg.split('').reverse().join('' )  //?!Hello computer (2)
-    },
-    set(newValue)  {
-     this.msg = newValue //detupmoc olleH  + !?  (1) 
+  computed : { 
+    reversedMessage() {
+      return this.msg.split('').reverse().join('')
     }
-   }
+  },
+  watch : { //데이터를 감시하는 용도의 옵션 
+    msg() {
+      console.log('msg:',this.msg) // msg : Good! 출력
+    },
+    reversedMessage () { // 계산된 데이터도 감시 가능
+      console.log('reversedMessage:', this.reversedMessage) // reversedMessage: !dooG 출력
+    }
   },
   methods : {
-    add() {
-      this.reversedMessage += '!?'
+    changeMessage() {
+      this.msg ='Good!'
     }
-  }
-  
+  },
 }
 </script>
