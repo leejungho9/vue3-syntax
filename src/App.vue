853768edcc720bb9f129b8a6cc94186753f7dca5
leejungho9 <9wjdgh9@naver.com>
2022-01-07 20:52:44 +0900

    Component Basics

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,34 @@
 <template>
-  <h1 @click="changeMessage">
-    {{ msg }}
+  <h1 @click="increase">
+    {{ count }} / {{ doubleCount }}
   </h1>
-  <h1> {{ reversedMessage }}</h1>
+  <h1>
+    {{ message }} / {{ reversedMessage }}
+  </h1>
 </template>
 
-<script>
+
+<script type= text/javascript > 
 export default {
-  data () {
-    return { 
-      msg : "Hello?"
+  data() {
+    return {
+      message : 'Hello world',
+      count : 0
     }
   },
-  computed : { 
-    reversedMessage() {
-      return this.msg.split('').reverse().join('')
-    }
-  },
-  watch : { //데이터를 감시하는 용도의 옵션 
-    msg() {
-      console.log('msg:',this.msg) // msg : Good! 출력
+  computed : {
+    doubleCount () {
+      return this.count * 2
     },
-    reversedMessage () { // 계산된 데이터도 감시 가능
-      console.log('reversedMessage:', this.reversedMessage) // reversedMessage: !dooG 출력
+    reversedMessage () {
+      return this.message.split('').reverse().join('')
     }
   },
   methods : {
-    changeMessage() {
-      this.msg ='Good!'
+    increase () {
+      this.count +=1
     }
-  },
+  }
 }
+
 </script>
---
+++ src/components/Child.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    Child :  {{ msg.value }}
+  </div>
+</template>
+
+<script> 
+    export default {
+       inject : ['msg']
+    }
+</script>
+
+
+<style scoped>
+
+</style>
